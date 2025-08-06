fe258c115ed360edbd1b55f9cfaa745d5bd16a30
lokax <m632656684@gmail.com>
2025-08-06 18:42:17 +0800

    debug log

--- src/tx_execution.cpp
+++ src/tx_execution.cpp
@@ -1996,7 +1996,6 @@ void TransactionExecution::Process(ReadOperation &read)
                 read_ts = ts;
             }
 
-            LOG(INFO) << "== ReadOperation";
             cc_handler_->Read(table_name,
                               read.read_tx_req_->schema_version_,
                               key,
