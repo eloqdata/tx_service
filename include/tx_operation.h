fd8ea9addbaf74fff5ff436ad4957e8367a0f057
lokax <m632656684@gmail.com>
2025-09-16 17:15:03 +0800

    test log

--- include/tx_operation.h
+++ include/tx_operation.h
@@ -412,7 +412,7 @@ struct ScanOpenOperation : TransactionOperation
 
 struct ScanState
 {
-    static constexpr size_t max_bucket_count_per_core = 20;
+    static constexpr size_t max_bucket_count_per_core = 40;
 
     ScanState() = delete;
     ScanState(std::unique_ptr<CcScanner> scanner,
