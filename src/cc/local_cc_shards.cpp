8279b00e1dd738aca302f9b930846651800fd1b1
Chen Zhao <ch3nzhao@gmail.com>
2025-09-26 21:26:58 +0800

    format

--- src/cc/local_cc_shards.cpp
+++ src/cc/local_cc_shards.cpp
@@ -5560,8 +5560,7 @@ void LocalCcShards::SyncTableStatisticsWorker()
         statistics_worker_ctx_.cv_.wait_for(
             worker_lk,
             10s,
-            [this]
-            {
+            [this] {
                 return statistics_worker_ctx_.status_ ==
                        WorkerStatus::Terminated;
             });
@@ -5832,8 +5831,7 @@ void LocalCcShards::HeartbeatWorker()
         bool wait_res = heartbeat_worker_ctx_.cv_.wait_for(
             heartbeat_worker_lk,
             std::chrono::seconds(1),
-            [this]
-            {
+            [this] {
                 return heartbeat_worker_ctx_.status_ ==
                        WorkerStatus::Terminated;
             });
@@ -5921,8 +5919,7 @@ void LocalCcShards::PurgeDeletedData()
         bool wait_res = purge_deleted_worker_ctx_.cv_.wait_for(
             worker_lk,
             std::chrono::seconds(10),
-            [this]
-            {
+            [this] {
                 return purge_deleted_worker_ctx_.status_ ==
                        WorkerStatus::Terminated;
             });
