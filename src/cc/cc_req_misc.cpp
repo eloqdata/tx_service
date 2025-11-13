a8135207d5568511321d6f4306b422364431f558
lokax <m632656684@gmail.com>
2025-11-13 03:30:03 +0000

    a

--- src/cc/cc_req_misc.cpp
+++ src/cc/cc_req_misc.cpp
@@ -546,6 +546,7 @@ void FillStoreSliceCc::Reset(const TableName &table_name,
     err_code_ = CcErrorCode::NO_ERROR;
     core_finished_.clear();
     core_finished_.resize(cc_shards.Count(), false);
+    err_code_ = CcErrorCode::NO_ERROR;
 }
 
 void FillStoreSliceCc::SetKvFinish(bool success)
