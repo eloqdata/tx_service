2dd7dc4fdcc2510d53952d1b1bb2cdec8f27e4d0
yi-xmu <yangshengwang2011@163.com>
2025-11-27 11:41:17 +0800

    fix assert

--- include/remote/remote_cc_request.h
+++ include/remote/remote_cc_request.h
@@ -121,7 +121,7 @@ public:
 
             if (core_cnt_ == 0 && !cce_addrs_.empty())
             {
-                assert(cce_addrs_.size() ==
+                assert(cce_addrs_.size() <=
                        Sharder::Instance().GetLocalCcShardsCount());
                 Acknowledge();
             }
