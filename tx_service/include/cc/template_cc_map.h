d30e2e1b48afb7fab7a731e2fc6288bd8a8def9b
Chen Zhao <ch3nzhao@gmail.com>
2025-12-10 18:14:31 +0800

    scan

--- tx_service/include/cc/template_cc_map.h
+++ tx_service/include/cc/template_cc_map.h
@@ -5975,6 +5975,7 @@ public:
                                 int32_t part_id =
                                     Sharder::MapKeyHashToHashPartitionId(
                                         key->Hash());
+                                LOG(WARNING) << "Scan FetchRecord";
                                 shard_->FetchRecord(table_name_,
                                                     table_schema_,
                                                     TxKey(key),
