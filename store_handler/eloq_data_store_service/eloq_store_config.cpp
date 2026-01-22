ecb6998bc1e7a08c5a9c787d9b198e77a2049f18
xiexiaoy <xiexiaoyang28@qq.com>
2026-01-22 18:07:20 +0800

    eloqstore: remove write limit (#375)

--- store_handler/eloq_data_store_service/eloq_store_config.cpp
+++ store_handler/eloq_data_store_service/eloq_store_config.cpp
@@ -124,9 +124,6 @@ DEFINE_bool(eloq_store_data_append_mode, true, "EloqStore data append mode.");
 DEFINE_bool(eloq_store_enable_compression,
             false,
             "EloqStore enable compression.");
-DEFINE_uint32(eloq_store_max_concurrent_writes,
-              1,
-              "EloqStore max write concurrency per core");
 DEFINE_uint32(eloq_store_max_upload_batch,
               100,
               "EloqStore max upload concurrency per core");
@@ -519,12 +516,6 @@ EloqStoreConfig::EloqStoreConfig(const INIReader &config_reader,
             : config_reader.GetBoolean("store",
                                        "eloq_store_enable_compression",
                                        FLAGS_eloq_store_enable_compression);
-    eloqstore_configs_.max_concurrent_writes =
-        !CheckCommandLineFlagIsDefault("eloq_store_max_concurrent_writes")
-            ? FLAGS_eloq_store_max_concurrent_writes
-            : config_reader.GetInteger("store",
-                                       "eloq_store_max_concurrent_writes",
-                                       FLAGS_eloq_store_max_concurrent_writes);
     eloqstore_configs_.max_upload_batch =
         !CheckCommandLineFlagIsDefault("eloq_store_max_upload_batch")
             ? FLAGS_eloq_store_max_upload_batch
