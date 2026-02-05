b49de7b7de2fe8573cd433baf075d3ecbd53141a
lokax <m632656684@gmail.com>
2026-02-05 17:45:17 +0800

    2

--- tx_service/src/cc/local_cc_shards.cpp
+++ tx_service/src/cc/local_cc_shards.cpp
@@ -132,9 +132,9 @@ LocalCcShards::LocalCcShards(
 
       data_sync_worker_ctx_(conf.at("core_num")),
 #ifdef EXT_TX_PROC_ENABLED
-      flush_data_worker_ctx_(1),
+      flush_data_worker_ctx_(2),
 #else
-      flush_data_worker_ctx_(1),
+      flush_data_worker_ctx_(2),
 #endif
       // cur_flush_buffers_ will be resized in StartBackgroudWorkers()
       // when worker_num_ is determined
