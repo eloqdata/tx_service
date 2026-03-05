dc8434c728fd9e66fb5d749a7d3ccb26ddfbddb4
yi-xmu <yangshengwang2011@163.com>
2026-03-05 18:47:08 +0800

    Update FillStoreSliceCc
    update key shard for load slice and fillslice

--- tx_service/include/cc/template_cc_map.h
+++ tx_service/include/cc/template_cc_map.h
@@ -7205,9 +7205,9 @@ public:
 
     bool Execute(FillStoreSliceCc &req) override
     {
-        std::deque<SliceDataItem> &slice_vec = req.SliceData(shard_->core_id_);
+        std::deque<SliceDataItem> &slice_vec = req.SliceData();
 
-        size_t index = req.NextIndex(shard_->core_id_);
+        size_t index = req.NextIndex();
         size_t last_index = std::min(index + FillStoreSliceCc::MaxScanBatchSize,
                                      slice_vec.size());
 
@@ -7224,11 +7224,12 @@ public:
         if (index == slice_vec.size())
         {
             slice_vec.clear();
-            return req.SetFinish(shard_);
+            req.SetFinish(shard_);
+            return true;
         }
         else
         {
-            req.SetNextIndex(shard_->core_id_, index);
+            req.SetNextIndex(index);
             shard_->Enqueue(shard_->LocalCoreId(), &req);
             return false;
         }
