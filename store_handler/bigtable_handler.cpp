3960a4752dd1bd55b695cfd41eeefe0e750529a6
lokax <m632656684@gmail.com>
2026-03-09 17:21:47 +0800

    fix heap order

--- store_handler/bigtable_handler.cpp
+++ store_handler/bigtable_handler.cpp
@@ -613,7 +613,9 @@ void EloqDS::BigTableHandler::FetchTableRanges(
             LocalCcShards *shards = Sharder::Instance().GetLocalCcShards();
             std::unique_lock<std::mutex> heap_lk(
                 shards->table_ranges_heap_mux_);
-            mi_override_thread(shards->GetTableRangesHeapThreadId());
+            bool is_override_thd = mi_is_override_thread();
+            mi_threadid_t prev_thd =
+                mi_override_thread(shards->GetTableRangesHeapThreadId());
             mi_heap_t *prev_heap =
                 mi_heap_set_default(shards->GetTableRangesHeap());
 
@@ -622,7 +624,14 @@ void EloqDS::BigTableHandler::FetchTableRanges(
                 mono_key.size());
 
             mi_heap_set_default(prev_heap);
-            mi_restore_default_thread_id();
+            if (is_override_thd)
+            {
+                mi_override_thread(prev_thd);
+            }
+            else
+            {
+                mi_restore_default_thread_id();
+            }
         }
 
         int32_t partition_id =
@@ -751,7 +760,9 @@ void EloqDS::BigTableHandler::OnFetchRangeSlices(
             LocalCcShards *shards = Sharder::Instance().GetLocalCcShards();
             std::unique_lock<std::mutex> heap_lk(
                 shards->table_ranges_heap_mux_);
-            mi_override_thread(shards->GetTableRangesHeapThreadId());
+            bool is_override_thd = mi_is_override_thread();
+            mi_threadid_t prev_thd =
+                mi_override_thread(shards->GetTableRangesHeapThreadId());
             mi_heap_t *prev_heap =
                 mi_heap_set_default(shards->GetTableRangesHeap());
 
@@ -766,7 +777,14 @@ void EloqDS::BigTableHandler::OnFetchRangeSlices(
             }
 
             mi_heap_set_default(prev_heap);
-            mi_restore_default_thread_id();
+            if (is_override_thd)
+            {
+                mi_override_thread(prev_thd);
+            }
+            else
+            {
+                mi_restore_default_thread_id();
+            }
             heap_lk.unlock();
         }
 
