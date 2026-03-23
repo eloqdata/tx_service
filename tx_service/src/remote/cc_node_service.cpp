211b0599ea006969dd869d88bfd6267b29250208
Kevin Chou <mrguin33@gmail.com>
2026-03-23 16:56:25 +0800

    tx_service: move standby ts checks into standby task lambdas

--- tx_service/src/remote/cc_node_service.cpp
+++ tx_service/src/remote/cc_node_service.cpp
@@ -1859,17 +1859,6 @@ void CcNodeService::UpdateStandbyCkptTs(
         response->set_error(true);
         return;
     }
-    if (request->primary_succ_ckpt_ts() <=
-        Sharder::Instance().NativeNodeGroupCkptTs())
-    {
-        DLOG(INFO) << "Discard UpdateStandbyCkptTs req, req ckpt ts:"
-                   << request->primary_succ_ckpt_ts()
-                   << ", own:" << Sharder::Instance().NativeNodeGroupCkptTs();
-        response->set_error(false);
-        response->set_current_ckpt_ts(
-            Sharder::Instance().NativeNodeGroupCkptTs());
-        return;
-    }
     auto store_hd = Sharder::Instance().GetLocalCcShards()->store_hd_;
     const bool has_data_store_write = request->has_data_store_write();
 
@@ -1883,6 +1872,15 @@ void CcNodeService::UpdateStandbyCkptTs(
          ckpt_ts,
          [store_hd, ng_id, ng_term, ckpt_ts, has_data_store_write]()
          {
+             const uint64_t current_ckpt_ts =
+                 Sharder::Instance().NativeNodeGroupCkptTs();
+             if (ckpt_ts <= current_ckpt_ts)
+             {
+                 DLOG(INFO) << "Discard UpdateStandbyCkptTs task, req ckpt ts:"
+                            << ckpt_ts << ", own:" << current_ckpt_ts;
+                 return;
+             }
+
              const bool ok =
                  store_hd == nullptr
                      ? true
@@ -2080,6 +2078,15 @@ void CcNodeService::RequestSyncSnapshot(
          snapshot_ts,
          [store_hd, ng_id, standby_term, term, snapshot_ts]()
          {
+             const uint64_t current_ckpt_ts =
+                 Sharder::Instance().NativeNodeGroupCkptTs();
+             if (snapshot_ts <= current_ckpt_ts)
+             {
+                 DLOG(INFO) << "Discard RequestSyncSnapshot task, snapshot_ts="
+                            << snapshot_ts << ", own:" << current_ckpt_ts;
+                 return;
+             }
+
              const bool ok =
                  store_hd->RequestSyncSnapshot(ng_id, term, snapshot_ts);
              if (ok)
